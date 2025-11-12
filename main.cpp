2e525ca4958cdede50c0ebf14aeba2f6e3e4b937
GhostByte26 <Gilbertomedina0526@icloud.com>
2025-11-12 10:19:10 -0700

    Initialized containers and begin loop logic implementation

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    // Array initialization and size restriction
+    int max_size = 100;
+    double nums[max_size];
+
+    // Containers initialization
+    double sum = 0;
+    double avg = 0;
+
+    // Input container
+    int count = 0;
+
+    // Data acquisition and loop
+    while (true)
+    {
+
+        std::cout << "Please enter the amount of numbers you would like to store: \n";
+        std::cin >> count;
+        // Clears the buffer
+        std::cin.ignore();
+    }
+}
